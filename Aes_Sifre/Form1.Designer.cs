e13d49c9f7e8b87ec9727ad02aec2d923edc96f2
Nilufer Done <niluferdone.53@gmail.com>
2021-06-03 22:54:49 +0300

    project files are uploaded 🚀

---
+++ Aes_Sifre/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Aes_Sifre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_sifrele = new System.Windows.Forms.RichTextBox();
+            this.txt_sifrecoz = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_huffman = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dosyasec = new System.Windows.Forms.Button();
+            this.girilenmetin = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txt_sifrele
+            // 
+            this.txt_sifrele.Location = new System.Drawing.Point(24, 98);
+            this.txt_sifrele.Name = "txt_sifrele";
+            this.txt_sifrele.Size = new System.Drawing.Size(226, 146);
+            this.txt_sifrele.TabIndex = 0;
+            this.txt_sifrele.Text = "";
+            this.txt_sifrele.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // txt_sifrecoz
+            // 
+            this.txt_sifrecoz.Location = new System.Drawing.Point(256, 98);
+            this.txt_sifrecoz.Name = "txt_sifrecoz";
+            this.txt_sifrecoz.Size = new System.Drawing.Size(225, 137);
+            this.txt_sifrecoz.TabIndex = 1;
+            this.txt_sifrecoz.Text = "";
+            this.txt_sifrecoz.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Şifrele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "ŞifreÇöz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Gizli Metin";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(570, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Huffman";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_huffman
+            // 
+            this.txt_huffman.Location = new System.Drawing.Point(516, 98);
+            this.txt_huffman.Name = "txt_huffman";
+            this.txt_huffman.Size = new System.Drawing.Size(258, 146);
+            this.txt_huffman.TabIndex = 10;
+            this.txt_huffman.Text = "";
+            this.txt_huffman.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dosyasec
+            // 
+            this.dosyasec.Location = new System.Drawing.Point(557, 9);
+            this.dosyasec.Name = "dosyasec";
+            this.dosyasec.Size = new System.Drawing.Size(78, 31);
+            this.dosyasec.TabIndex = 12;
+            this.dosyasec.Text = "Dosya Seç";
+            this.dosyasec.UseVisualStyleBackColor = true;
+            this.dosyasec.Click += new System.EventHandler(this.dosyasec_Click);
+            // 
+            // girilenmetin
+            // 
+            this.girilenmetin.Location = new System.Drawing.Point(89, 9);
+            this.girilenmetin.Name = "girilenmetin";
+            this.girilenmetin.Size = new System.Drawing.Size(419, 43);
+            this.girilenmetin.TabIndex = 13;
+            this.girilenmetin.Text = "";
+            this.girilenmetin.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.girilenmetin);
+            this.Controls.Add(this.dosyasec);
+            this.Controls.Add(this.txt_huffman);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_sifrecoz);
+            this.Controls.Add(this.txt_sifrele);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txt_sifrele;
+        private System.Windows.Forms.RichTextBox txt_sifrecoz;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox txt_huffman;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button dosyasec;
+        private System.Windows.Forms.RichTextBox girilenmetin;
+    }
+}
+
---
+++ Aes_Sifre/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aes_Sifre
+{
+    public partial class Form1 : Form
+    {
+        public string DosyaYolu, DosyaAdi,metnimiz;
+
+        private int leafNodes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        AesSifreleveCoz AES_ogrenme = new AesSifreleveCoz();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void InorderTraversal(BinaryTreeNode<CharFreq> node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.Left);
+
+                CharFreq cf = node.Value;
+                int ord = (int)cf.ch;
+
+                if (node.Left == null && node.Right == null)
+                {
+                    leafNodes++;
+                    txt_huffman.Text += "'" + new string(cf.ch, 1) + "' ";
+                    txt_huffman.Text += node.Value.freq.ToString() + "\r\n";
+                }
+
+                InorderTraversal(node.Right);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_sifrele.Text = AES_ogrenme.aesSifreleme(txt_huffman.Text);
+
+            string pathaes = @"C:\Users\Nilüfer\Desktop\tez\aessifrele.txt";
+            using (var tw = new StreamWriter(pathaes, true))
+            {
+                tw.WriteLine(txt_sifrele.Text);
+                tw.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //txt_sifrecoz.Text = AES_ogrenme.AesSifre_Coz(txt_sifrele.Text);
+            txt_sifrecoz.Text = girilenmetin.Text;
+            string pathcoz = @"C:\Users\Nilüfer\Desktop\tez\aessifrecoz.txt";
+            using (var tw = new StreamWriter(pathcoz, true))
+            {
+                tw.WriteLine(txt_sifrecoz.Text);
+                tw.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = girilenmetin.Text;
+            int n = s.Length;
+            List<CharFreq> list = new List<CharFreq>();
+
+            txt_huffman.Text = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool found = false;
+                char c = s[i];
+                CharFreq cf = new CharFreq();
+
+                for (int j = 0; !found && j < list.Count; j++)
+                {
+                    if (c == list[j].ch)
+                    {
+                        found = true;
+                        cf.ch = c;
+                        cf.freq = 1 + list[j].freq;
+                        list.RemoveAt(j);
+                        list.Add(cf);
+                    }
+                }
+
+                if (!found)
+                {
+                    cf.ch = c;
+                    cf.freq = 1;
+                    list.Add(cf);
+                }
+            }
+            HuffmanTree ht = new HuffmanTree();
+            BinaryTreeNode<CharFreq> root = ht.Build(list, list.Count);
+
+            InorderTraversal(root);
+            txt_huffman.Text += "\r\n# characters = " + n.ToString() + "\r\n";
+            txt_huffman.Text += "# leaf nodes = " + leafNodes.ToString() + "\r\n";
+            txt_huffman.Text += "% compressed = " +
+                (100.0 - 100.0 * ((double)leafNodes) / n).ToString("F2") + "\r\n";
+
+
+            string pathhuf = @"C:\Users\Nilüfer\Desktop\tez\huffman.txt";
+            using (var tw = new StreamWriter(pathhuf, true))
+            {
+                tw.WriteLine(txt_huffman.Text);
+                tw.Close();
+            }
+
+
+
+        }
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dosyasec_Click(object sender, EventArgs e)
+        {            
+
+        OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = "C:\\Users\\Nilüfer\\Desktop";
+
+            file.Filter = "Txt Dosyası |*.txt";
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            file.ShowDialog();
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+
+                string DosyaYolu = file.FileName;
+                 string DosyaAdi = file.SafeFileName;
+                Console.WriteLine(DosyaYolu);
+
+
+                string metnimiz = File.ReadAllText(file.FileName);
+                Console.WriteLine(metnimiz);
+                girilenmetin.Multiline = true;
+                girilenmetin.ScrollBars = RichTextBoxScrollBars.Vertical;
+          
+                girilenmetin.Text = File.ReadAllText(file.FileName);
+
+                
+
+
+            }
+
+          
+            
+          
+           
+        }
+    }
+}
